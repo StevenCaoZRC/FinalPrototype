81d8561a775739b8adb898ef71b7d387c3c320b8
Steven <stevenzhaooooo@gmail.com>
2019-03-25 17:07:14 +1300

    Camera Panning, snaps back when your not panning

--- Prototype-3/Assets/Scripts/CameraFollow.cs
+++ Prototype-3/Assets/Scripts/CameraFollow.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform m_target;
-    public float m_smoothing = 5.0f;
-
+    public float m_smoothing = 10.0f;
     Vector3 m_offset;
 
     // Start is called before the first frame update
@@ -14,11 +13,16 @@ public class CameraFollow : MonoBehaviour
     {
         m_offset = transform.position - m_target.position;
     }
-
     void FixedUpdate()
     {
+
         Vector3 targetCameraPos = m_target.position + m_offset;
 
-        transform.position = Vector3.Lerp(transform.position, targetCameraPos, Time.deltaTime * m_smoothing);
+        if (Input.GetAxis("CamHorizontal") == 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetCameraPos, Time.deltaTime * m_smoothing);
+        }
+        
+        //transform.LookAt(m_target);
     }
 }
---
+++ Prototype-3/Assets/Scripts/CameraPanning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanning : MonoBehaviour
+{
+    public Transform m_targetLocation;
+  
+    private float m_panSpeed = 10f;
+    private float m_panLimit = 8f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //m_panLimit = m_targetLocation.position.x + 10;
+        Vector3 camPos = transform.position;
+
+
+        if (Input.GetAxis("CamHorizontal") > 0)
+        {
+            camPos.x += m_panSpeed * Time.deltaTime;
+        }
+        else if(Input.GetAxis("CamHorizontal") < 0)
+        {
+            camPos.x -= m_panSpeed * Time.deltaTime;
+        }
+        
+        camPos.x = Mathf.Clamp(camPos.x, m_targetLocation.position.x - m_panLimit, m_targetLocation.position.x + m_panLimit);
+        
+   
+        transform.position = camPos;
+    }
+}
