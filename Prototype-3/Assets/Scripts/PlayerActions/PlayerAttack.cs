3705c64318a00605e8a3c3a2386524671c4bf6b1
Steven <stevenzhaooooo@gmail.com>
2019-03-28 11:08:19 +1300

    Tried to do katana VFX application

--- Prototype-3/Assets/Scripts/PlayerActions/PlayerAttack.cs
+++ Prototype-3/Assets/Scripts/PlayerActions/PlayerAttack.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public GameObject m_katana;
+    public GameObject m_particles;
+    public Transform m_slashingPoint;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && m_katana.activeSelf)
+        {
+          
+                GameObject temp1 = Instantiate(m_particles, m_slashingPoint.position, Quaternion.Euler(0,-90,0)); //Quaternion.Euler(90, m_slashingPoint.rotation.y, m_slashingPoint.rotation.z));
+                Destroy(temp1, 1);
 
+         
+          
+           
+         
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "DestructibleObject" && Input.GetKeyDown(
-           KeyCode.E) && m_katana.activeSelf)
+        if (other.gameObject.tag == "DestructibleObject" && Input.GetKeyDown(KeyCode.E) && m_katana.activeSelf)
         {
+            
             other.gameObject.GetComponent<DestructibleObject>().m_iHealth -= 2;
             if (other.gameObject.GetComponent<DestructibleObject>().m_iHealth <= 0)
             {
                 Destroy(other.gameObject);
             }
+            
         }
     }
 }
