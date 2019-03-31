578b09783de51b535067e8f0880df84bc1245c75
SugarZzzzzz <31192307+SugarZzzzzz@users.noreply.github.com>
2019-03-31 22:26:19 +1300

    Arrow Trap Done just need assets for it, Not modified to be enabled by pressure plates, and only shoots one at a time instead of 3

---
+++ Prototype-3/Assets/Scripts/Traps/ArrowTrap/ArrowMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+    //This script is assigned to the prefab of the arrow
+    [SerializeField]
+    private float m_speed;
+    [SerializeField]
+    public float m_fireRate;
+
+    private Vector3 m_pos;
+    private Vector3 m_direction;
+    private Quaternion m_rotation;
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_speed != 0)
+        {
+            transform.position += transform.forward * (m_speed * Time.deltaTime);
+        
+        }
+        else {
+            Debug.Log("Arrow is Not Moving");
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag != "ArrowTrap")
+        {
+            m_speed = 0;
+            Destroy(gameObject);
+        }
+       
+    }
+
+
+}
---
+++ Prototype-3/Assets/Scripts/Traps/ArrowTrap/SpawnArrows.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArrows : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_arrow;
+    [SerializeField]
+    GameObject m_firePoint;
+    public bool m_trapActive = true;
+    private float timeToFire = 0;
+    void Start()
+    {
+        //Uncomment this and use this if Time.time breaks in reloading scene
+        //InvokeRepeating("Spawn", 1, 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Comment all of this if your gonna use InvokeRepeating.
+        if(m_trapActive && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1 / m_arrow.GetComponent<ArrowMove>().m_fireRate;
+            Spawn();
+        }
+        
+    }
+    void Spawn()
+    {
+        GameObject arrow;
+
+        if (m_firePoint != null)
+        {
+            arrow = Instantiate(m_arrow, m_firePoint.transform.position, m_firePoint.transform.rotation);
+
+        }
+        else
+        {
+            Debug.LogError("The Fire Point is Missing");
+        }
+
+    }
+  
+}
