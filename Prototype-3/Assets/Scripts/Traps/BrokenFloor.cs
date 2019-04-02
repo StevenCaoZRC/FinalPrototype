a0f92fafe31399c6368a1bde7f0303881714de31
Steven <stevenzhaooooo@gmail.com>
2019-04-03 12:33:21 +1300

    Added Broken Floor

--- Prototype-3/Assets/Scripts/Traps/BrokenFloor.cs
+++ Prototype-3/Assets/Scripts/Traps/BrokenFloor.cs
@@ -6,6 +6,9 @@ public class BrokenFloor : MonoBehaviour
 {
     [SerializeField] private GameObject m_leftPivot;
     [SerializeField] private GameObject m_rightPivot;
+    [SerializeField] private Transform m_leftEndRot;
+    [SerializeField] private Transform m_rightEndRot;
+    [SerializeField] private float m_speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +20,20 @@ public class BrokenFloor : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(OpenTrap());
+        }
+    }
     IEnumerator OpenTrap()
     {
-
-        m_leftPivot.transform.rotation = Quaternion.Lerp(m_leftPivot.transform.rotation, m_leftEnd.rotation, m_speed * Time.deltaTime);
-        m_rightPivot.transform.rotation = Quaternion.Lerp(m_rightPivot.transform.rotation, m_rightEnd.rotation, m_speed * Time.deltaTime);
-        
-      
-
+        yield return new WaitForSeconds(1);
+        m_leftPivot.transform.rotation = Quaternion.Lerp(m_leftPivot.transform.rotation, m_leftEndRot.rotation, m_speed * Time.deltaTime);
+        m_rightPivot.transform.rotation = Quaternion.Lerp(m_rightPivot.transform.rotation, m_rightEndRot.rotation, m_speed * Time.deltaTime);
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
         yield return null;
     }
 }
