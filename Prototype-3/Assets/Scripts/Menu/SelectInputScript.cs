8094ff6dce10dfc1c64da27f2d4d2c2a74dc49d3
Steven <stevenzhaooooo@gmail.com>
2019-04-04 11:38:35 +1300

    Added Menu, completed and works with keyboard and mouse controls

---
+++ Prototype-3/Assets/Scripts/Menu/SelectInputScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectInputScript : MonoBehaviour
+{
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool bButtonSelected;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        eventSystem.SetSelectedGameObject(selectedObject);
+        bButtonSelected = true;
+
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) && !bButtonSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            bButtonSelected = true;
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+            eventSystem.SetSelectedGameObject(selectedObject);
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    private void OnEnable()
+    {
+        eventSystem.SetSelectedGameObject(selectedObject);
+        bButtonSelected = true;
+    }
+}
